cde2255c784675b706b501bf04e40886ed843ba5
khushbuv84 <khushbuv84@gmail.com>
2018-12-18 16:08:43 +0530

    added notes page

--- crepes4u/src/app/app-routing.module.ts
+++ crepes4u/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotesComponent} from  './notes/notes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
