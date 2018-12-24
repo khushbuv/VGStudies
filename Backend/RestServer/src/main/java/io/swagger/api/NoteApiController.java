edaed0615e104e7f7c5d4e7264d858143a6e3732
khushbuv84 <khushbuv84@gmail.com>
2018-12-24 18:44:43 +0530

    changed name to frontend and added docs schema

--- Backend/RestServer/src/main/java/io/swagger/api/NoteApiController.java
+++ Backend/RestServer/src/main/java/io/swagger/api/NoteApiController.java
@@ -1,8 +1,9 @@
 package io.swagger.api;
 
-import io.swagger.model.Note;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,16 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.Note;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-12-22T05:19:55.468Z")
 
 @Controller
@@ -39,7 +36,9 @@ public class NoteApiController implements NoteApi {
 
     public ResponseEntity<Void> addNote(@ApiParam(value = "",required=true) @PathVariable("userId") Long userId,@ApiParam(value = "Note object that needs to be added" ,required=true )  @Valid @RequestBody Note body) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        System.out.println("added a note for, userid: "+userId);
+        System.out.println("note: "+body);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     public ResponseEntity<Void> deleteNote(@ApiParam(value = "note id to delete",required=true) @PathVariable("noteId") Long noteId,@NotNull @ApiParam(value = "user id to delete associated note", required = true) @Valid @RequestParam(value = "userId", required = true) Long userId) {
